7415e25274dbc22c6e6ad4d160486453551de5bd
evensky1 <fromthedeepsize@gmail.com>
2022-11-23 22:44:13 +0300

    Finite automaton parser was implemented

--- StringFormatter.Tests/UnitTest.cs
+++ StringFormatter.Tests/UnitTest.cs
@@ -1,3 +1,6 @@
+using StringFormatter.impl;
+using StringFormatter.impl.Automaton;
+
 namespace StringFormatter.Tests;
 
 public class Tests
@@ -10,6 +13,10 @@ public class Tests
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        var fa = new FiniteAutomaton();
+        var temp = fa.ParseString("Привет,{{ }}{FirstName} {LastName}!");
+        foreach (var token in temp) Console.WriteLine($"Type: {token.Type} value: {token.Value}");
+        
+        Assert.That(true, Is.True);
     }
 }
---
+++ StringFormatter/impl/Automaton/FiniteAutomaton.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+using StringFormatter.impl.Exception;
+
+namespace StringFormatter.impl.Automaton;
+
+public class FiniteAutomaton
+{
+    
+    private readonly AutomatonState[,] _transitionMatrix = 
+    {
+        { AutomatonState.PreFieldName, AutomatonState.Shield, AutomatonState.Exception, AutomatonState.Init, AutomatonState.Exception, AutomatonState.Init, AutomatonState.Init},
+        { AutomatonState.PreShield, AutomatonState.Exception, AutomatonState.EndField, AutomatonState.Init, AutomatonState.Shield, AutomatonState.Init,  AutomatonState.Init},
+        { AutomatonState.Init, AutomatonState.FieldName, AutomatonState.FieldName, AutomatonState.Init, AutomatonState.Exception, AutomatonState.Init,  AutomatonState.Init}
+    };
+
+    private AutomatonState _currentState;
+
+    public FiniteAutomaton()
+    {
+        _currentState = AutomatonState.Init;
+    }
+
+    public List<Token> ParseString(string str)
+    {
+        var tokens = new List<Token>();
+        var tokenValue = new StringBuilder();
+        
+        foreach (var c in str)
+        {
+            //Console.Write($"{_currentState} -> {c} -> ");
+            _currentState = GetNextState(c);
+            //Console.WriteLine($"{_currentState}");
+            switch (_currentState)
+            {
+                case AutomatonState.Init:
+                    tokenValue.Append(c);
+                    break;
+                
+                case AutomatonState.PreFieldName:
+                    tokens.Add(new Token(TokenType.String, tokenValue.ToString()));
+                    tokenValue.Clear();
+                    break;
+                
+                case AutomatonState.FieldName:
+                    tokenValue.Append(c);
+                    break;
+                
+                case AutomatonState.EndField:
+                    tokens.Add(new Token(TokenType.Substitution, tokenValue.ToString()));
+                    tokenValue.Clear();
+                    _currentState = AutomatonState.Init;
+                    break;
+
+                case AutomatonState.PreShield:
+                    break;
+                
+                case AutomatonState.Shield:
+                    tokens.Add(new Token(TokenType.String, c.ToString()));
+                    tokenValue.Clear();
+                    _currentState = AutomatonState.Init;
+                    break;
+                case AutomatonState.Exception:
+                    throw new InvalidStringException();
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        return tokens;
+    }
+
+    
+    private AutomatonState GetNextState(char symbol)
+    {
+        _currentState = symbol switch
+        {
+            '{' => _transitionMatrix[0, (int)_currentState],
+            '}' => _transitionMatrix[1, (int)_currentState],
+            _ => _transitionMatrix[2, (int)_currentState]
+        };
+
+        return _currentState;
+    }
+}
